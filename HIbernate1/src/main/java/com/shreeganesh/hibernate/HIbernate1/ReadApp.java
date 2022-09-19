140eb9964ad82ecde40ffa013e991cb785b07ae5
Krishna Gupta <krishnagupta24697@gmail.com>
2022-09-19 16:04:21 +0530

    Entity states during read operation

--- HIbernate1/src/main/java/com/shreeganesh/hibernate/HIbernate1/ReadApp.java
+++ HIbernate1/src/main/java/com/shreeganesh/hibernate/HIbernate1/ReadApp.java
@@ -14,15 +14,16 @@ public class ReadApp
 
     	Session s=sessionFactory.openSession();
     	s.beginTransaction();
-    	Song s1 = s.get(Song.class,1);
+    	Song s1 = s.get(Song.class,1);//s1 in persistent state
     	s.getTransaction().commit();
     	SessionFactory sessionFactory1 = HibernateUtils.getSessionFactory();
 
     	Session session=sessionFactory1.openSession();
     	session.beginTransaction();
-    	Song s2 = session.load(Song.class,2);
+    	Song s2 = session.load(Song.class,2);//s2 in persistent state
     	session.getTransaction().commit();
-    	
+    	s.close();//s1 in detached state
+    	session.close();//s2 in detached state
     	System.out.println(s1);
     	System.out.println(s2);
     	
