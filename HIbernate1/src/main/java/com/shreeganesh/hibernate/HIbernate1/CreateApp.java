bf1b9f7adaa7d75451a39cc25b0c613fa8f65426
Krishna Gupta <krishnagupa24697@gmail.com>
2022-09-19 15:53:49 +0530

    Added @GeneratedValue and Understanding Entity States
    used Identity as id generating strategy.
    Understanding transient, persistent and detached entity states

--- HIbernate1/src/main/java/com/shreeganesh/hibernate/HIbernate1/CreateApp.java
+++ HIbernate1/src/main/java/com/shreeganesh/hibernate/HIbernate1/CreateApp.java
@@ -15,12 +15,17 @@ public class CreateApp
     	Session s=sessionFactory.openSession();
     	
     	Song s1 = new Song();
-    	s1.setId(2);
-    	s1.setSongName("Khamoshiyan");
-    	s1.setSinger("Arjeet");
+    	s1.setSongName("Khamoshiyan - New");
+    	s1.setSinger("Arjeet Singhal");
+    	System.out.println("Sesion contains song obj : "+s.contains(s1));//s1 in Transient state
     	s.beginTransaction();
     	s.save(s1);
+    	System.out.println("Sesion contains song obj : "+s.contains(s1));//s1 in persistent state
     	s.getTransaction().commit();
+    	s.evict(s1);
+    	System.out.println("Sesion contains song obj : "+s.contains(s1));//s1 in detached state
+    	
+    	s.close();
     	System.out.println("Row Added");
     }
 }
--- HIbernate1/src/main/java/com/shreeganesh/hibernate/HIbernate1/dto/Song.java
+++ HIbernate1/src/main/java/com/shreeganesh/hibernate/HIbernate1/dto/Song.java
@@ -2,18 +2,26 @@ package com.shreeganesh.hibernate.HIbernate1.dto;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity(name = "songs")
+@Entity
+@Table(name = "songs")
 public class Song {
 
 	@Id
 	@Column(name = "songId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	@Column(name = "songName")
 	private String songName;
+	
 	@Column(name = "artist")
 	private String singer;
+	
 	public Integer getId() {
 		return id;
 	}
